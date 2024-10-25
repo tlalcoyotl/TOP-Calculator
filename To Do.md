719581a96bc9a3d317062ed2af7df318f2bd424a
tlalcoyotl <92064829+tlalcoyotl@users.noreply.github.com>
2024-10-25 02:54:31 -0600

    Get most functionality of calculator going

---
+++ To Do.md
@@ -0,0 +1,10 @@
+To Do
+For Calculator
+Display text should show what is being input
+    Display content = button pressed
+    Function to pick up pressed number into value
+    should it be an array that gets joined and made into a number?
+    Or i could store both in an object
+    And either this array.join() gets sent to display or the object property is read
+
+number 2 should always be cleared up and their operation result stored in num 1
--- js/script.js
+++ js/script.js
@@ -1,6 +1,82 @@
-let num1 = 0;
-let num2 = 0;
-let operator;
+let num1 = [];
+let num2 = [];
+let isNum2Storing = false;
+let operator = '';
+let display = document.querySelector('#display');
+const buttons = document.querySelector('#buttons')
+
+buttons.addEventListener('click', (e) => {
+        if (e.target.id == 'clear') {
+            num1 = [];
+            num2 = [];
+            isNum2Storing = false;
+            operator = '';
+            display.textContent = '';
+        } else if (typeof parseInt(e.target.textContent) == 'number' 
+        && e.target.className == 'number') {
+            numPush(e);
+    } else if (e.target.parentElement.className == 'operators') {
+        if (isNum2Storing = false) {
+            getOperatorNum1(e);
+            isNum2Storing = true; 
+        } else {
+            getOperatorNum2(e);
+        };
+        }
+    }
+)
+
+function numPush(e) {
+    if (isNum2Storing === false) {
+        num1.push(e.target.textContent);
+        display.textContent = num1.join('');
+    } else {
+        num2.push(e.target.textContent);
+        display.textContent = num2.join('');
+    }
+}
+
+function getOperatorNum1(e) {
+    switch(e.target.id) {
+        case ('plus'):
+            operator = '+';
+            break;
+        case ('minus'):
+            operator = '-';
+            break;
+        case ('mult'):
+            operator = '*'
+            break;
+        case ('division'):
+            operator = '/';
+            break;
+        case ('equal'):
+            return;
+    }
+}
+
+function getOperatorNum2(e) {
+    switch(e.target.id) {
+        case ('plus'):
+            operate(num1.join(''), num2.join(''), operator)
+            operator = '+';
+            break;
+        case ('minus'):
+            operate(num1.join(''), num2.join(''), operator)
+            operator = '-';
+            break;
+        case ('mult'):
+            operate(num1.join(''), num2.join(''), operator)
+            operator = '*'
+            break;
+        case ('division'):
+            operate(num1.join(''), num2.join(''), operator)
+            operator = '/';
+            break;
+        case ('equal'):
+            operate(num1.join(''), num2.join(''), operator);
+    }
+}
 
 function add(a, b) {
     return parseInt(a) + parseInt(b);
@@ -15,22 +91,24 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if (b === 0) {return 'ERROR'}
+    if (b === 0) {return 'ERROR'};
     return parseInt(a) / parseInt(b);
 }
 
 function operate(n1, n2, opr) {
     switch(opr) {
         case ('+'):
-            return add(n1, n2);
+            num1 = add(n1, n2).toString().split('');
             break;
         case ('-'):
-            return substract(n1, n2);
+            num1 = substract(n1, n2).toString().split('');
             break;
         case ('*'):
-            return multiply(n1, n2);
+            num1 = multiply(n1, n2).toString().split('');
             break;
         case ('/'):
-            return divide(n1, n2);
+            num1 = divide(n1, n2).toString.split('');
     }
+    display.textContent = num1.join('');
+    num2 = [];
 }
